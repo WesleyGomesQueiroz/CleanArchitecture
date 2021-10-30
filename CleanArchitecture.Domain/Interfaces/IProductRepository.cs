df3f022cf32443052f5433ea182f287f6c9030d8
wesle <wesle@LAPTOP-EFB6LC9O>
2021-10-30 13:59:30 -0300

    Interfaces repository

---
+++ CleanArchitecture.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using CleanArchitecture.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+
+        Task<Product> GetByIdAsync(int? id);
+
+        Task<Product> GetProducCategoryAsync(int? id);
+
+        Task<Product> CreateAsync(Product category);
+        
+        Task<Product> UpdateAsync(Product category);
+        
+        Task<Product> RemoveAsync(Product category);
+        
+    }
+}
