a72fe1ec3b19688adebdd74983490aa90cbeaee8
wesle <wesle@LAPTOP-EFB6LC9O>
2021-10-31 17:47:54 -0300

    Commands and queries

---
+++ CleanArchitecture.Application/Products/Queries/GetProductByQuery.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitecture.Domain.Entities;
+using MediatR;
+
+namespace CleanArchitecture.Application.Products.Queries
+{
+    public class GetProductByQuery : IRequest<Product>
+    {
+        public int Id { get; set; }
+        
+        public GetProductByQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
