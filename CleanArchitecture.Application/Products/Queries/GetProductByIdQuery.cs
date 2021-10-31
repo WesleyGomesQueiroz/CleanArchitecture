36b3f7a78814b884ef771a32d512aa703fc35789
wesle <wesle@LAPTOP-EFB6LC9O>
2021-10-31 19:02:51 -0300

    Implement handler

--- CleanArchitecture.Application/Products/Queries/GetProductByIdQuery.cs
+++ CleanArchitecture.Application/Products/Queries/GetProductByIdQuery.cs
@@ -3,11 +3,11 @@ using MediatR;
 
 namespace CleanArchitecture.Application.Products.Queries
 {
-    public class GetProductByQuery : IRequest<Product>
+    public class GetProductByIdQuery : IRequest<Product>
     {
         public int Id { get; set; }
         
-        public GetProductByQuery(int id)
+        public GetProductByIdQuery(int id)
         {
             Id = id;
         }
