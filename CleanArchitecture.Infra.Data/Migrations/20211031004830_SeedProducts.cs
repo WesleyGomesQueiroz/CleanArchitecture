0dc62fedbc021363c7f33bc2ee6afdd60789ec74
wesle <wesle@LAPTOP-EFB6LC9O>
2021-10-31 09:59:34 -0300

    Migrations

---
+++ CleanArchitecture.Infra.Data/Migrations/20211031004830_SeedProducts.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArchitecture.Infra.Data.Migrations
+{
+    public partial class SeedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId) " +
+                "VALUES('Caderno espiral', 'Caderno espiral 100 folhas', 7.45, 50, 'caderno1.jpg', 1)");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE FROM Products");
+        }
+    }
+}
