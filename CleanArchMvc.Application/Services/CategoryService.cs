c3f9da37d3c61cc54c09601a9ad3da377e732bda
Wesley Queiroz <wesleygomesqueiroz@gmail.com>
2024-01-02 18:53:04 -0300

    Implementando o serviço para Category - CategoryService

---
+++ CleanArchMvc.Application/Services/CategoryService.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryService(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<CategoryDTO>> GetCategories()
+        {
+            var categoriesEntity = await _categoryRepository.GetCategories();
+            return _mapper.Map<IEnumerable<CategoryDTO>>(categoriesEntity);
+        }
+
+        public async Task<CategoryDTO> GetById(int? id)
+        {
+            var categoriesEntity = await _categoryRepository.GetById(id);
+            return _mapper.Map<CategoryDTO>(categoriesEntity);
+        }
+
+        public async Task Add(CategoryDTO categoryDTO)
+        {
+            var categoriesEntity = _mapper.Map<Category>(categoryDTO);
+            await _categoryRepository.Create(categoriesEntity);
+        }
+
+        public async Task Update(CategoryDTO categoryDTO)
+        {
+            var categoriesEntity = _mapper.Map<Category>(categoryDTO);
+            await _categoryRepository.Update(categoriesEntity);
+        }
+
+        public async Task Delete(int id)
+        {
+            var categoriesEntity = _categoryRepository.GetById(id).Result;
+            await _categoryRepository.Delete(categoriesEntity);
+        }
+    }
+}
