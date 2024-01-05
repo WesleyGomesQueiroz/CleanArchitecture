d138e385b5116214af9670ddb82c8cfa7123bae5
Wesley Queiroz <wesleygomesqueiroz@gmail.com>
2024-01-04 21:40:14 -0300

    CQRS - Testando a implementação - II

--- CleanArchMvc.Application/Services/ProductService.cs
+++ CleanArchMvc.Application/Services/ProductService.cs
@@ -1,8 +1,11 @@
 ﻿using AutoMapper;
 using CleanArchMvc.Application.DTOs;
 using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Application.Products.Queries;
 using CleanArchMvc.Domain.Entities;
 using CleanArchMvc.Domain.Interfaces;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -10,49 +13,57 @@ namespace CleanArchMvc.Application.Services
 {
     public class ProductService : IProductService
     {
-        private IProductRepository _productRepository;
+        //private IProductRepository _productRepository;
         private readonly IMapper _mapper;
+        private readonly IMediator _mediator;
 
-        public ProductService(IProductRepository productRepository, IMapper mapper)
+        public ProductService(/*IProductRepository productRepository,*/ IMapper mapper, IMediator mediator)
         {
-            _productRepository = productRepository;
+            //_productRepository = productRepository;
             _mapper = mapper;
+            _mediator = mediator;
         }
 
         public async Task<IEnumerable<ProductDTO>> GetProducts()
         {
-            var productsEntity = await _productRepository.GetProductsAsync();
-            return _mapper.Map<IEnumerable<ProductDTO>>(productsEntity);
+            var productsQuery = new GetProductsQuery();
+
+            if (productsQuery == null)
+                throw new Exception($"Entity could not be loaded.");
+
+            var result = await _mediator.Send(productsQuery);
+
+            return _mapper.Map<IEnumerable<ProductDTO>>(result);
         }
 
-        public async Task<ProductDTO> GetById(int? id)
-        {
-            var productsEntity = await _productRepository.GetByIdAsync(id);
-            return _mapper.Map<ProductDTO>(productsEntity);
-        }
+        //public async Task<ProductDTO> GetById(int? id)
+        //{
+        //    var productsEntity = await _productRepository.GetByIdAsync(id);
+        //    return _mapper.Map<ProductDTO>(productsEntity);
+        //}
 
-        public async Task<ProductDTO> GetProductCategory(int? id)
-        {
-            var productsEntity = await _productRepository.GetProductCategoryAsync(id);
-            return _mapper.Map<ProductDTO>(productsEntity);
-        }
+        //public async Task<ProductDTO> GetProductCategory(int? id)
+        //{
+        //    var productsEntity = await _productRepository.GetProductCategoryAsync(id);
+        //    return _mapper.Map<ProductDTO>(productsEntity);
+        //}
 
-        public async Task Add(ProductDTO productDto)
-        {
-            var productEntity = _mapper.Map<Product>(productDto);
-            await _productRepository.CreateAsync(productEntity);
-        }
+        //public async Task Add(ProductDTO productDto)
+        //{
+        //    var productEntity = _mapper.Map<Product>(productDto);
+        //    await _productRepository.CreateAsync(productEntity);
+        //}
 
-        public async Task Update(ProductDTO productDto)
-        {
-            var productEntity = _mapper.Map<Product>(productDto);
-            await _productRepository.UpdateAsync(productEntity);
-        }
+        //public async Task Update(ProductDTO productDto)
+        //{
+        //    var productEntity = _mapper.Map<Product>(productDto);
+        //    await _productRepository.UpdateAsync(productEntity);
+        //}
 
-        public async Task Delete(int? id)
-        {
-            var productsEntity = await _productRepository.GetByIdAsync(id);
-            await _productRepository.DeleteAsync(productsEntity);
-        }
+        //public async Task Delete(int? id)
+        //{
+        //    var productsEntity = await _productRepository.GetByIdAsync(id);
+        //    await _productRepository.DeleteAsync(productsEntity);
+        //}
     }
 }
