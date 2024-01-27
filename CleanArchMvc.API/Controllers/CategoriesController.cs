01089d7b8bfc4747cbc672f4ff242947217a943d
Wesley Queiroz <wesleygomesqueiroz@gmail.com>
2024-01-27 16:14:13 -0300

    HttpGet{id} - Retornando uma Categoria pelo seu Id

--- CleanArchMvc.API/Controllers/CategoriesController.cs
+++ CleanArchMvc.API/Controllers/CategoriesController.cs
@@ -29,5 +29,17 @@ namespace CleanArchMvc.API.Controllers
             }
             return Ok(categories);
         }
+
+        [HttpGet("id")]
+        public async Task<ActionResult<CategoryDTO>> Get( int id)
+        {
+            var category = await _categoryService.GetById(id);
+            if (category == null)
+            {
+                return NotFound("Category not found");
+            }
+            return Ok(category);
+        }
+
     }
 }
