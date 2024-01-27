d4f69b7996409579d40540c3b02eb60db53f89b5
Wesley Queiroz <wesleygomesqueiroz@gmail.com>
2024-01-27 16:42:50 -0300

    HttpDelete - Excluindo uma Categoria pelo seu Id

--- CleanArchMvc.API/Controllers/CategoriesController.cs
+++ CleanArchMvc.API/Controllers/CategoriesController.cs
@@ -68,5 +68,20 @@ namespace CleanArchMvc.API.Controllers
             return Ok(categoryDTO);
         }
 
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<CategoryDTO>> Delete(int id)
+        {
+            var category = await _categoryService.GetById(id);
+            
+            if (category == null)
+            {
+                return NotFound("Category not found");
+            }
+
+            await _categoryService.Delete(id);
+
+            return Ok(category);
+        }
+
     }
 }
