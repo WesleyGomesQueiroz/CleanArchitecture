51666d61cbbeb3f08677fccda19b5951c4ed5f33
Wesley Queiroz <wesleygomesqueiroz@gmail.com>
2024-01-27 16:07:06 -0300

    HettpGet  - Retornando todas as Categorias

--- CleanArchMvc.API/Controllers/CategoriesController.cs
+++ CleanArchMvc.API/Controllers/CategoriesController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace CleanArchMvc.API.Controllers
 {
@@ -7,5 +12,22 @@ namespace CleanArchMvc.API.Controllers
     [ApiController]
     public class CategoriesController : ControllerBase
     {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> Get()
+        {
+            var categories = await _categoryService.GetCategories();
+            if (categories == null)
+            {
+                return NotFound("Categories not found");
+            }
+            return Ok(categories);
+        }
     }
 }
